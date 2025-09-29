3e98b0ec630731c6094738bde0a5230d90c02397
JP <jacobpwarsaw@gmail.com>
2025-09-29 13:00:23 -0700

    Added education section and associated files

---
+++ src/data/education.js
@@ -0,0 +1,32 @@
+export const education = [
+  {
+    id: 1,
+    degree: "Master of Science in Computer Science & Engineering",
+    school: "University of California, Santa Cruz",
+    location: "Santa Cruz, CA",
+    startDate: "2023",
+    endDate: "Jan 2025",
+    gpa: null,
+    specialization: "Machine Learning, Computer Networking",
+    coursework: ["Machine Learning", "Computer Networking", "Advanced Algorithms", "Research Methods"],
+    achievements: ["M.S. Capstone Project: S-BLE Seat Position Classifier"],
+    type: "graduate"
+  },
+  {
+    id: 2,
+    degree: "Bachelor of Science in Computer Science & Engineering",
+    minor: "Mathematics",
+    school: "University of California, Santa Cruz",
+    location: "Santa Cruz, CA",
+    startDate: "2019",
+    endDate: "June 2023",
+    gpa: "3.85",
+    coursework: ["Data Structures & Algorithms", "Database Systems", "Software Engineering", "Computer Architecture", "Operating Systems"],
+    achievements: [
+      "Graduated Cum Laude",
+      "Highest Honors in Major",
+      "Organizations: ΑΕΠ (Alpha Epsilon Pi), ΔΣΠ (Delta Sigma Pi)"
+    ],
+    type: "undergraduate"
+  }
+];
